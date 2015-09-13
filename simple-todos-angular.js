e68170bd435d6a34fc061087cc68ebebe4d5c9e6
Jesse Copeland <jesse@sudokrew.com>
2015-09-12 14:59:05 -1000

    Changes needed for running on iOS.

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -3,6 +3,16 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   angular.module('simple-todos',['angular-meteor']);
 
+  function onReady () {
+    angular.bootstrap(document, ['simple-todos']);
+  }
+
+  if (Meteor.isCordova) {
+    angular.element(document).on('deviceready', onReady);
+  } else {
+    angular.element(document).ready(onReady);
+  }
+
   angular.module('simple-todos')
     .controller('TodosListCtrl', ['$scope', '$meteor',
       function ($scope, $meteor) {
